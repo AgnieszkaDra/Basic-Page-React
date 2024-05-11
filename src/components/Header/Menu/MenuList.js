2554199bc2480b7db0ddc2206a9e7e6da1510fca
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-05-11 21:13:45 +0200

    refactor nav

--- src/components/Header/Menu/MenuList.js
+++ src/components/Header/Menu/MenuList.js
@@ -4,89 +4,88 @@ import { HeaderDataTeamContext } from '../../context';
 import { useScroll } from '../../../hooks/useScroll';
 
 export const MenuTree = ({ isNavbarOpen }) => {
-  const { elements } = useContext(HeaderDataTeamContext);
-  const [menu, setMenu] = useState(elements);
-  const root = elements[0];
-  const menuIds = root.childIds;
-
-  const MenuItemText = ( {id, menuElementById} ) => {
-    const menuElement = menuElementById[id];
-    const childIds = menuElement.childIds;
-    console.log(childIds)
-    const [showChilds, setShowChilds] = useState(false);
-
-    return (
-        <div
-        className='menu__sublist__item--text'
-          
-          style={{height: '100%'}}
-        >
-         <div>{menuElement.name} </div>
-       
-          
-        </div>
-      );
-
-  }
+    const { elements } = useContext(HeaderDataTeamContext);
+    const [menu, setMenu] = useState(elements);
+    const root = elements[0];
+    const menuIds = root.childIds;
+    const [showSublist, setShowSublist] = useState(false);
 
 
+    const MenuItemText = ( {id, menuElementById} ) => {
+        const menuElement = menuElementById[id];
+        const childIds = menuElement.childIds;
+        
+        return (
+            <div 
+                className='menu__sublist__item--text'
+                style={{height: '100%'}}
+            >
+            <div>{menuElement.name}</div>
+            </div>
+        );
+    }
 
-  const MenuList = ({ id, menuElementById }) => {
-    const menuElement = menuElementById[id];
-    const childIds = menuElement.childIds;
-    const [showChilds, setShowChilds] = useState(false);
+    const MenuList = ({ id, menuElementById }) => {
+            const menuElement = menuElementById[id];
+            const childIds = menuElement.childIds;
+        
+            
+            return (
+            <li
+                className="menu__listItem menu"
+                onMouseEnter={() => setShowSublist(true)}
+            
+            >
+                <div>{menuElement.name} </div>
+                {showSublist && childIds.length > 0 && (
+                <ul 
+                    className='menu__sublist' 
+                    style={{ zIndex: 200 }}
+                    onMouseLeave={() => setShowSublist(false)}
+                >
+                    {childIds.map(childId => (
+                    <MenuSubList
+                        key={childId}
+                        id={childId}
+                        parentId={id}
+                        menuElementById={menuElementById}
+                    />
+                    ))}
+                </ul>
+                )}
+            </li>
+            );
+        }
+
+    const MenuSubList = ({ id, menuElementById }) => {
+        const menuElement = menuElementById[id];
+        const childIds = menuElement.childIds;
+        const [showMenuTextItem, setShowMenuTextItem] = useState(false);
+        return (
+            <li
+                className="menu__sublist__item"
+                onMouseEnter={() => setShowMenuTextItem(true)}
+            >
+            <div>{menuElement.name}</div>
+            {showMenuTextItem && childIds.length > 0 && (
+                <ul 
+                    className='menu__text' 
+                    style={{ zIndex: 200 }}
+                >
+                    {childIds.map(childId => (
+                    <MenuItemText
+                        key={childId}
+                        id={childId}
+                        parentId={id}
+                        menuElementById={menuElementById}
+                    />
+                    ))}
+            </ul>
+            )}
+        </li>
+        );
+    }
     
-    return (
-      <li
-        className="menu__listItem menu"
-        onMouseEnter={() => setShowChilds(true)}
-        // onMouseLeave={() => setShowChilds(false)}
-      >
-        <div>{menuElement.name} </div>
-        {showChilds && childIds.length > 0 && (
-          <ul className='menu__sublist' style={{ zIndex: 200 }}>
-            {childIds.map(childId => (
-              <MenuSubList
-                key={childId}
-                id={childId}
-                parentId={id}
-                menuElementById={menuElementById}
-              />
-            ))}
-          </ul>
-        )}
-      </li>
-    );
-  }
-
-  const MenuSubList = ({ id, menuElementById }) => {
-    const menuElement = menuElementById[id];
-    const childIds = menuElement.childIds;
-    const [showChilds, setShowChilds] = useState(false);
-    
-    return (
-      <li
-        className="menu__sublist__item menu"
-        onMouseEnter={() => setShowChilds(true)}
-        onMouseLeave={() => setShowChilds(false)}
-      >
-        <div>{menuElement.name} </div>
-        {showChilds && childIds.length > 0 && (
-          <ul className='menu__text' style={{ zIndex: 200 }}>
-            {childIds.map(childId => (
-              <MenuItemText
-              className='menu__sublist__Item'
-                key={childId}
-                id={childId}
-                parentId={id}
-                menuElementById={menuElementById}
-              />
-            ))}
-          </ul>
-        )}
-      </li>
-    );
-  }
   return (
     <ul className={`nav__menu menu ${isNavbarOpen ? 'nav__hide' : 'nav__block'}`}>
       {menuIds.map(id => (
